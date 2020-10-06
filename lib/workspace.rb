7989599cca4d5c73dde5d11d64181cc5aa36b239
Christabel Sebastian <christabel.escarez@gmail.com>
2020-10-06 15:28:44 -0700

    Added positive nominal #load_channels test

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -1,5 +1,7 @@
 require_relative '../test/test_helper'
+
 CONVERSATIONS_LIST_URL = "https://slack.com/api/conversations.list"
+
 class Workspace
   attr_reader :users, :channels
   def initialize
@@ -8,26 +10,26 @@ class Workspace
   end
 
   def load_channels
+
     query = {
-        token: ENV["SLACK_TOKEN"]
+      token: ENV['SLACK_TOKEN']
     }
 
     request = error_message(HTTParty.get(CONVERSATIONS_LIST_URL, query: query))
 
-    if request.class == String
-      raise ArgumentError, request
-    end
+    raise ArgumentError, request if request.class == String
+
     @channels = request["channels"].map { |channel| channel["name"] }
   end
 
   def error_message(response)
-
     if response.code != 200 || response["ok"] != true
       return "API request failed with error code #{response.code} and #{response["error"]}."
     else
       return response
     end
   end
+
 end
 
 
--- test/workspace_test.rb
+++ test/workspace_test.rb
@@ -2,15 +2,35 @@ require_relative 'test_helper'
 
 
 describe "workspace class" do
-  describe "workspace instantiation" do
-    it "is an instance of workspace" do
-      new_workspace = Workspace.new
-      expect(new_workspace).must_be_instance_of Workspace
-    end
-  end
-  describe "load channels" do
-    it "loads channels" do
 
+  describe "workspace instantiation" do
+    before do
+      @new_workspace = Workspace.new
+    end
+
+    it "is an instance of workspace" do
+      expect(@new_workspace).must_be_instance_of Workspace
+    end
+
+    it "establishes the base data structures when instantiated" do
+      [:users, :channels].each do |keyword|
+        expect(@new_workspace).must_respond_to keyword
+      end
+
+      expect(@new_workspace.users).must_be_kind_of Array
+      expect(@new_workspace.channels).must_be_kind_of Array
+    end
+
+  end
+
+  describe "load channels" do
+    before do
+      @new_workspace = Workspace.new
+    end
+
+    it "loads channels" do
+      @new_workspace.load_channels
+      expect(@new_workspace.channels).wont_be_empty
     end
 
   end
