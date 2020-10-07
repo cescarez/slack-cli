89aacbfa47fabb24112dec6809de5ef7e22ced42
Christabel Sebastian <christabel.escarez@gmail.com>
2020-10-07 16:25:59 -0700

    Added (and commented out) VCR config line to allow HTTP requests outside of VCR control blocks

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,17 +18,14 @@ Dotenv.load
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 VCR.configure do |config|
-  config.cassette_library_dir = "test/cassettes"
-  config.hook_into :webmock
-end
-
-VCR.configure do |config|
   config.cassette_library_dir = "test/cassettes" # folder where casettes will be located
   config.hook_into :webmock # tie into this other tool called webmock
   config.default_cassette_options = {
     :record => :new_episodes,    # record new data when we don't have it yet
     :match_requests_on => [:method, :uri, :body], # The http method, URI and body of a request all need to match
   }
+
+  # config.allow_http_connections_when_no_cassette = true #added by Rachael and Christabel to allow to HTTP Requests without cassettes
 
   # Don't leave our token lying around in a cassette file.
   config.filter_sensitive_data("<SLACK_TOKEN>") do
