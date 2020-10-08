074a9ee457311e78f30c7aa76ca8c035a63ebd92
Christabel Sebastian <christabel.escarez@gmail.com>
2020-10-07 22:33:05 -0700

    Added driver code to send message to channel or user

--- lib/slack.rb
+++ lib/slack.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 require 'dotenv'
+require 'awesome_print'
 require_relative 'workspace'
 
 def main
@@ -11,8 +12,8 @@ def main
   workspace.load_channels
   puts "There are #{workspace.channels.length} channels and #{workspace.users.length} users."
 
-  puts "1. list users\n2. list channels\n3. select user\n4. select channel\n5. quit"
-  selection = number_validation(gets.chomp, 5)
+  puts selection_items
+  selection = get_selection(gets.chomp)
 
   while (1..4).include? selection
     case selection
@@ -23,34 +24,69 @@ def main
     when 3
       puts "Please input a username or slack id"
       selected_user = workspace.select_user(gets.chomp)
-      puts "Would you like details?"
-      details_selection = input_validation(gets.chomp.downcase)
-      if details_selection == "yes"
-        puts workspace.show_details(selected_user)
+
+      if selected_user
+        puts "Would you like details about #{selected_user.real_name}?"
+        puts get_details(workspace, selected_user)
+
+        puts "Would you like to send #{selected_user.real_name} a message? (yes/no)"
+        send_message(selected_user)
+      else
+        puts "No user found."
       end
     when 4
       puts "Please input a channel name or slack id"
       selected_channel = workspace.select_channel(gets.chomp)
-      puts "Would you like details?"
-      details_selection = input_validation(gets.chomp.downcase)
-      if details_selection == "yes"
-        puts workspace.show_details(selected_channel)
-      end
 
+      if selected_channel
+        puts "Would you like details?"
+        puts get_details(workspace, selected_channel)
+
+        puts "Would you like to post on the ##{selected_channel.name} channel? (yes/no)"
+        send_message(selected_channel)
+      else
+        puts "No channel found"
+      end
     end
 
     puts "Make another selection:"
-    puts "1. list users\n2. list channels\n3. select user\n4. select channel\n5. quit"
-    selection = number_validation(gets.chomp, 5)
+    puts selection_items
+    selection = get_selection(gets.chomp)
   end
 
   puts "Thank you for using the Ada Slack CLI"
 end
 
-def number_validation(input, max_num)
+def selection_items
+  return "1. list users\n2. list channels\n3. select user\n4. select channel\n5. quit"
+end
+
+def get_details(workspace, instance)
+  details_selection = input_validation(gets.chomp.downcase)
+
+  if details_selection == "yes"
+    return workspace.show_details(instance)
+  end
+end
+
+def send_message(instance)
+  if instance.class == User
+  elsif instance.class == Channel
+  end
+
+  message_selection = input_validation(gets.chomp.downcase)
+
+  if message_selection == "yes"
+    puts "Please enter your message:"
+    message = gets.chomp
+    instance.send_message(message)
+  end
+end
+
+def get_selection(input)
   input = translate_input(input)
 
-  while !(1..max_num).include? input
+  unless (1..5).include? input
     puts "Invalid input. Please re-enter a selection."
     input = translate_input(gets.chomp)
   end
